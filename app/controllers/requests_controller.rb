66a215876884cf5737c1faa4e9f3d4d9962c5f71
Sibylle de Courcel <sibylle44@gmail.com>
2017-02-28 19:16:26 +0100

    show and update requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,9 +1,29 @@
 class RequestsController < ApplicationController
 
   skip_before_action :authenticate_user!, only: :index
+  before_action :set_request, only: [:show, :update]
 
   def index
     @requests = Request.all
   end
+
+  def show
+  end
+
+  def update
+    @request.rider = current_user
+    if @request.save
+      redirect_to rider_requests_path
+    else
+      render 'requests/index'
+    end
+  end
+
+  private
+
+  def set_request
+    @request = Request.find(params[:id])
+  end
+
 
 end
