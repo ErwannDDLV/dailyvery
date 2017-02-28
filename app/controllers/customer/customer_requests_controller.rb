7f2fbf4eec90166f209a0789c822dc073c194996
Eric Piboyeux <ericpiboyeux@gmail.com>
2017-02-28 15:46:42 +0100

    before merging commit

---
+++ app/controllers/customer/customer_requests_controller.rb
@@ -0,0 +1,15 @@
+class Customer::RequestsController < RequestsController
+
+  def index
+    current_user = User.find(1)
+    @requests = current_user.requests
+    byebug
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+end
