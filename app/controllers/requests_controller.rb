32a9ae0f65e412c91abc20d5a142f4fd7d6bc12c
Sibylle de Courcel <sibylle44@gmail.com>
2017-03-01 16:01:09 +0100

    map loaded with markers

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,6 +5,19 @@ class RequestsController < ApplicationController
 
   def index
     @requests = Request.all
+    @departures = Location.joins("INNER JOIN requests ON locations.id = requests.departure_id")
+                         .near('12 cours du Medoc, 33000 Bordeaux', 5)
+                        #.where.not(latitude: nil, longitude: nil)
+                       #.where.not("locations.latitude = ? AND locations.longitude = ?", nil, nil)
+
+
+
+    @hash = Gmaps4rails.build_markers(@departures) do |departure, marker|
+      marker.lat departure.latitude
+      marker.lng departure.longitude
+      marker.infowindow render_to_string(partial: "/requests/map_box", locals: { departure: departure })
+end
+
   end
 
   def show
