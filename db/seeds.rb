6ff10778e7f31f41275fcd03cfca1a81908f2a29
Sibylle de Courcel <sibylle44@gmail.com>
2017-03-01 14:44:36 +0100

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 Request.destroy_all
 User.destroy_all
+Location.destroy_all
 
 users = [
   {
@@ -51,75 +52,91 @@ locations = [
     address: '20 cours Pasteur, 33000 Bordeaux'
   },
   {
-    address:
+    address: '10 cours Saint Louis, 33000 Bordeaux'
   },
   {
-    address:
+    address: '8 impasse Barreyre Deuxième, 33000 Bordeaux'
   },
   {
-    address:
+    address: '5 rue Robert d\'Ennery, 33200 Bordeaux'
   },
   {
-    address:
+    address: '28 rue de Pessac, 33000 Bordeaux'
   },
   {
-    address:
+    address: '14 rue Notre Dame, 33000 Bordeaux'
   },
   {
-    address:
+    address: '1719 Allée Margaux, 33000 Bordeaux'
   },
   {
-    address:
+    address: '115 cours Balguerie Stuttenberg, 33200 Bordeaux'
   },
   {
-    address:
+    address: '1 rue André Messager, 33400 Talence'
   },
   {
-    address:
+    address: '263 avenue de Tivoli, 33110 Le Bouscat'
+  },
+  {
+    address: '20 rue du Petit Cardinal, 33100 Bordeaux'
+  },
+  {
+    address: '80 rue des Sablières, 33800 Bordeaux'
   }
 ]
 
+loc_arr = []
+locations.each do |loc|
+  loc_new = Location.new(loc)
+  if loc_new.save
+    puts "location id: #{loc_new.id} OK"
+    loc_arr << loc_new
+  end
+end
+
+
 requests = [
   {
-    departure: '20 cours Pasteur, 33000 Bordeaux',
-    arrival: '10 cours Saint Louis, 33000 Bordeaux',
+    departure: loc_arr[0],
+    arrival: loc_arr[1],
     description: '4 chemises à récupérer au pressing',
     parcel: 1,
     customer: users_arr[0]
   },
   {
-    departure: '8 impasse Barreyre Deuxième, 33000 Bordeaux',
-    arrival: '5 rue Robert d\'Ennery, 33200 Bordeaux',
+    departure: loc_arr[2],
+    arrival: loc_arr[3],
     description: 'sac de sport',
     parcel: 1,
     customer: users_arr[1]
   },
   {
-    departure: '28 rue de Pessac, 33000 Bordeaux',
-    arrival: '14 rue Notre Dame, 33000 Bordeaux',
+    departure: loc_arr[4],
+    arrival: loc_arr[5],
     description: 'cle à molette',
     parcel: 1,
     customer: users_arr[1],
     rider: users_arr[2]
     },
   {
-    departure: '1719 Allée Margaux, 33000 Bordeaux',
-    arrival: '115 cours Balguerie Stuttenberg, 33200 Bordeaux',
+    departure: loc_arr[6],
+    arrival: loc_arr[7],
     description: 'panier de legumes',
     parcel: 1,
     customer: users_arr[2],
     rider: users_arr[1]
     },
   {
-    departure: '1 rue André Messager, 33400 Talence',
-    arrival: '263 avenue de Tivoli, 33110 Le Bouscat',
+    departure: loc_arr[8],
+    arrival: loc_arr[9],
     description: 'chaussures chez le coordonnier',
     parcel: 1,
     customer: users_arr[4]
     },
   {
-    departure: '20 rue du Petit Cardinal, 33100 Bordeaux',
-    arrival: '80 rue des Sablières, 33800 Bordeaux',
+    departure: loc_arr[10],
+    arrival: loc_arr[11],
     description: 'panier AMAP',
     parcel: 1,
     customer: users_arr[0],
