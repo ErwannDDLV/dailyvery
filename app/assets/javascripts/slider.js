703cd0935795794cc62e0bd3e46e6a020d0a1106
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2017-03-07 14:46:42 +0100

    slider nearly finished

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,5 +1,19 @@
-function outputUpdate(vol) {
-  var output = document.querySelector("#volume");
-  output.value = vol;
-  output.style.left = vol + 'px';
-}
+// function outputUpdate(vol) {
+//   var output = document.querySelector("#volume");
+//   output.value = vol;
+//   output.style.left = vol + 'px';
+// }
+
+$( function() {
+  $( "#slider" ).slider({
+    value: 3,
+    min: 1,
+    max: 10,
+    step: 1,
+    slide: function( event, ui ) {
+      var valueTest = ui.value;
+      $( ".ui-slider-handle" ).text(valueTest + ' km');
+      $( "#search_distance" ).val(valueTest);
+    },
+  });
+} );
